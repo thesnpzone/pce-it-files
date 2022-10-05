db80274e489e8334e6cc573a29840d00173517e6
snpzone <65709045+snpzone@users.noreply.github.com>
2022-10-05 19:22:42 +0530

    added required in file module

--- models/Fileupload.js
+++ models/Fileupload.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const Filesupload = mongoose.Schema({
-    title: String,
-    desc: String,
-    filelink: String,
+    title: { type: String, required: true },
+    desc: { type: String, required: true },
+    filelink: { type: String, required: true },
 });
 
 module.exports = mongoose.model("file", Filesupload);
