e0a9263b66cdddac51d37e2600b43a867c39ff2f
snpzone <65709045+snpzone@users.noreply.github.com>
2022-10-05 19:03:10 +0530

    added cors

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 
 
