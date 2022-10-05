ea6cf806b8b2633ad7ec588b0b7910c8ff022435
snpzone <65709045+snpzone@users.noreply.github.com>
2022-10-05 19:27:02 +0530

    added required in file module defin cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
